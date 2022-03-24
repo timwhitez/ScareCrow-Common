3682314cc485c191e7b9907a8ea9ebaf2affa397
TimWhite <36320909+timwhitez@users.noreply.github.com>
2022-03-24 15:20:22 +0800

    Delete process_injection directory

--- process_injection/injection/injection.go
+++
@@ -1,9 +0,0 @@
-package injection
-
-func main() {
-
-}
-
-func NtAllocate(callid uint16, PHandle uint64, BaseA, ZeroBits, RegionSize, AllocType, Protect uintptr, nothing uint64) uintptr
-
-func NtProtectVirtualMemory(callid uint16, argh ...uintptr) (errcode uint32, err error)
